3f74315631a3d0ddbffcbd2c4a75bf2693255f1c
zattaoui <03zaki95@gmail.com>
2022-03-08 15:03:05 +0100

    Create routes with NavBar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import {RouterModule, Routes} from "@angular/router";
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { DeviceAndAppComponent } from './device-and-app/device-and-app.component';
+import { ScientificValidationComponent } from './scientific-validation/scientific-validation.component';
+
+const appRoutes: Routes = [
+  { path: 'auth', component: AuthComponent },
+  { path: 'home', component: HomeComponent},
+  { path: 'deviceApp', component: DeviceAndAppComponent},
+  { path: 'scientificValidation', component: ScientificValidationComponent},
+  { path: 'aboutUs', component: AboutUsComponent},
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    HomeComponent,
+    AboutUsComponent,
+    DeviceAndAppComponent,
+    ScientificValidationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
